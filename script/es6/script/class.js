df93ed7b2d48892e05af79e8325bfd74b45d8ddf
user <pedano.alessandro@gmail.com>
2017-01-23 22:54:17 +0100

    Aggiunti i file degli script ECMA Scritp 5 e 6

---
+++ script/es6/script/class.js
@@ -0,0 +1,22 @@
+// Base Class ES6
+class Uccello {
+	constructor(peso, altezza){
+		this.peso = peso;
+		this.altezza = altezza;
+	}
+	
+	cammina(){
+		console.log("cammina!");
+	}
+}
+
+//Subclass
+class Pinguino extends Uccello {
+	constructor(peso, altezza){
+		super(peso, altezza);
+	}
+	
+	nuota(){
+		console.log("nuota");
+	}
+}
