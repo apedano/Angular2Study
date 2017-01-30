b7adc661e57089abde0f5fa214a20ee8a6f1136e
user <pedano.alessandro@gmail.com>
2017-01-30 22:45:33 +0100

    Modificata l'app con gli array dei nomi

--- first-app/app/app.component.ts
+++ first-app/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello names</h1>
+	<ul><li *ngFor="let name of names" >Ciao {{name}}</li></ul>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	names : string[]; 
+	
+	constructor(){
+			this.names = ['Grazia', 'Graziella', 'Grazie tante'];
+	}
+}
